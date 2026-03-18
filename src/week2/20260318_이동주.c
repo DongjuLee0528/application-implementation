a31cfd8520049c2ef7a8fe650b98160e7c2dc9bc
DongjuLee <dozoo0528@gmail.com>
2026-03-18 10:33:47 +0900

    2주 수업 1

--- src/week2/20260318_이동주.c
+++ src/week2/20260318_이동주.c
@@ -1,3 +1,14 @@
 //
 // Created by Dongju Lee on 2026. 3. 18..
 //
+#include <stdio.h>
+
+int main(void)
+{
+    printf("\t Hello C! \n Hello World! \n");
+    printf("큰따옴표: \" \" \n");
+    printf("작은따옴표: \' \' \n");
+    printf("역슬레시: \\\\ \n");
+
+    return 0;
+}
