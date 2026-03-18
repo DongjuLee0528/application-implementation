e96bea0c5805f49701e2db3ce339a155304df5c2
DongjuLee <dozoo0528@gmail.com>
2026-03-18 11:44:01 +0900

    2주 수업 4

---
+++ src/week2/2026031804_이동주.c
@@ -0,0 +1,16 @@
+//
+// Created by Dongju Lee on 2026. 3. 18..
+//
+#include <stdio.h>
+
+int main(void)
+{
+    printf("10진수 정수: %d \n", 0.5);
+    printf("10진수 실수: %f \n", 0.5);
+    printf("10진수 실수: %lf \n", 0.5);
+
+    printf("소수점 이하 6자리이상: %f \n", 0.5655678);
+    printf("소수점 이하 6자리이상: %lf \n", 0.5667784);
+
+    return 0;
+}
