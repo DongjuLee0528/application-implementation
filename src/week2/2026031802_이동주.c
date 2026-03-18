b82113972b5ea8ed747ef7e18d746420363b7cce
DongjuLee <dozoo0528@gmail.com>
2026-03-18 10:50:04 +0900

    2주 수업 2

---
+++ src/week2/2026031802_이동주.c
@@ -0,0 +1,13 @@
+//
+// Created by Dongju Lee on 2026. 3. 18..
+//
+
+#include <stdio.h>
+int main(void) {
+    printf("%d 더하기 %d는 %d 입니다 \n", 3,5,3+5);
+    printf("%i 더하기 %i는 %i 입니다 \n", 3,5,3+5);
+    printf("%d - %d = %d입니다 \n",3,5,3-5);
+    printf("%i - %i = %i입니다 \n",3,5,3-5);
+
+    return 0;
+}
