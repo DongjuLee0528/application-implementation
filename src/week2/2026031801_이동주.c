a5137faf4feb945e33ba17c8e9c37ec48c8c3319
DongjuLee <dozoo0528@gmail.com>
2026-03-18 11:26:16 +0900

    2주 수업

--- src/week2/2026031801_이동주.c
+++ src/week2/2026031801_이동주.c
@@ -10,5 +10,10 @@ int main(void)
     printf("작은따옴표: \' \' \n");
     printf("역슬레시: \\\\ \n");
 
+    printf("%d 더하기 %d는 %d 입니다 \n", 3,5,3+5);
+    printf("%i 더하기 %i는 %i 입니다 \n", 3,5,3+5);
+    printf("%d - %d = %d입니다 \n",3,5,3-5);
+    printf("%i - %i = %i입니다 \n",3,5,3-5);
+
     return 0;
 }
