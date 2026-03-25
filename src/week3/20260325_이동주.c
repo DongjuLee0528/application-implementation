48672e1c1689764b9de69afaa43574d66002943c
DongjuLee <dozoo0528@gmail.com>
2026-03-25 10:19:05 +0900

    3주차

--- src/week3/20260325_이동주.c
+++ src/week3/20260325_이동주.c
@@ -1,3 +1,17 @@
 //
 // Created by Dongju Lee on 2026. 3. 25..
 //
+/*
+#include <stdio.h>
+
+int main(void)
+{
+    int age;
+
+    printf("What is your age?: ");
+    scanf("%d", &age);
+
+    printf("Wow! Really? Are you %d years old?\n", age);
+
+    return 0;
+}*/
