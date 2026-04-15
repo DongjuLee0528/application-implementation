18a1f9d23dace664350bb626de4a1e85bfb76352
DongjuLee <dozoo0528@gmail.com>
2026-04-15 14:01:01 +0900

    7주차

---
+++ src/week07/homework.c
@@ -0,0 +1,17 @@
+//
+// Created by Dongju Lee on 2026. 4. 15..
+//
+#include <stdio.h>
+
+int main(void)
+{
+    char ch = 'A';
+
+    while (ch <= 'Z')
+    {
+        printf("%c\n", ch);
+        ch++;
+    }
+
+    return 0;
+}
