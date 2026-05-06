136f65632499682ac0b9f22fe0769e2e7bb598b7
DongjuLee <dozoo0528@gmail.com>
2026-05-06 11:03:12 +0900

    10주차

--- src/week10/20260506_이동주.c
+++ src/week10/20260506_이동주.c
@@ -1,3 +1,25 @@
 //
 // Created by Dongju Lee on 2026. 5. 6..
 //
+#include <stdio.h>
+
+#include <stdio.h>
+
+int sum(int x, int y)
+{
+    int result = 0;
+    result = x + y;
+
+    return result;
+}
+
+int main(void)
+{
+    int answer = 0;
+
+    answer = sum(3, 4);
+
+    printf("%d \n", answer);
+
+    return 0;
+}
